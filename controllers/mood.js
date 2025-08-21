b932910e7ec93c7ecd8c9a5ec74ce14b153d2b88
vartika malik <vartika06malik@gmail.com>
2025-08-21 15:04:22 +0530

    Storing mood data in the backend

--- controllers/mood.js
+++ controllers/mood.js
@@ -1,8 +1,11 @@
-const{allMoods} = require("../public/js/moodQuestions.js");
-const{moodStyles} = require("../public/js/moodStyles.js")
-const{moodActivities} = require("../public/js/moodActivities.js")
-const {moodHeadings,moodDescription} = require("../public/js/moodHeadings.js")
-
+const { allMoods } = require("../public/js/moodQuestions.js");
+const { moodStyles } = require("../public/js/moodStyles.js");
+const { moodActivities } = require("../public/js/moodActivities.js");
+const {
+  moodHeadings,
+  moodDescription,
+} = require("../public/js/moodHeadings.js");
+const User = require('../models/user.js');
 
 module.exports.renderChooseMood = (req, res) => {
   res.render("pages/mood.ejs");
@@ -18,10 +21,41 @@ module.exports.renderMoodQues = (req, res) => {
   let myMoodStyle = moodStyles[mood];
   res.render("pages/reasons.ejs", { mood, myMood, myMoodStyle });
 };
-module.exports.moodQues = (req, res) => {
-  let mood = req.params.moodName;
-  res.redirect(`/mood/${mood}/activities`);
+module.exports.moodQues = async (req, res) => {
+  try {
+    const mood = req.params.moodName; 
+    const userId = req.user._id; 
+    const { reasons } = req.body;
+    console.log(mood)
+    console.log(userId)
+    console.log(reasons)
+    if (!userId || !mood || !reasons) {
+      return res.status(400).json({ message: "Missing required data." });
+    }
+
+    const user = await User.findById(userId);
+
+    if (!user) {
+      return res.status(404).json({ message: "User not found." });
+    }
+
+    const newMoodEntry = {
+      mood: mood,
+      reasons: reasons, 
+      date: new Date(),
+    };
+
+    user.moods.push(newMoodEntry);
+
+    await user.save();
+
+    res.redirect(`/mood/${mood}/activities`);
+  } catch (error) {
+    console.error("Error saving mood:", error);
+    res.status(500).json({ message: "Internal server error." });
+  }
 };
+
 module.exports.renderMoodActivities = (req, res) => {
   let mood = req.params.moodName;
   let myMoodStyle = moodStyles[mood];
--- models/user.js
+++ models/user.js
@@ -10,7 +10,20 @@ const userSchema = new Schema({
     email : {
         type:String,
         required : true,
-    }
+    },
+    moods: [{
+        mood: {
+            type: String, // e.g., 'Happy', 'Sad', 'Anxious'
+            required: true,
+        },
+        reasons: [{
+            type: String, // An array to hold multiple reasons
+        }],
+        date: {
+            type: Date,
+            default: Date.now,
+        },
+    }]
 })
 
 userSchema.plugin(passportLocalMongoose);
